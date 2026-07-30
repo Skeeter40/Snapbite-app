81e120cda988117056db3c1e5d5eec15f10b1bd2
Skeeter40 <skiapauldo@yahoo.com>
2026-07-30 19:08:01 -0400

    Update create-checkout-session.js

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -8,6 +8,9 @@ export default async function handler(req, res) {
   }
 
   try {
+    if (!process.env.STRIPE_PRICE_ID) {
+      return res.status(500).json({ error: "PRICE_ID_MISSING: " + JSON.stringify(process.env.STRIPE_PRICE_ID) });
+      }
     const session = await stripe.checkout.sessions.create({
       mode: "subscription",
       line_items: [
